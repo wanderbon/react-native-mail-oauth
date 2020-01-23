2fed7fff87c97898107c84133828f6856fea897f
Evgeniy Kozhich <wanderbon95@gmail.com>
2020-01-23 14:31:31 +0300

    fix

--- android/src/main/java/com/wanderbon/mailouath/MailOauthModule.java
+++ android/src/main/java/com/wanderbon/mailouath/MailOauthModule.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Looper;
 import android.os.Handler;
-import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Promise;
--- android/src/main/java/com/wanderbon/mailouath/SDKResultCallback.java
+++ android/src/main/java/com/wanderbon/mailouath/SDKResultCallback.java
@@ -3,17 +3,13 @@ package com.wanderbon.mailouath;
 import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableMap;
-
-import java.util.Map;
 
 import ru.mail.auth.sdk.AuthResult;
 import ru.mail.auth.sdk.MailRuCallback;
 
 class SDKResultCallback implements MailRuCallback {
     private Promise resultPromise;
-    private WritableMap result;
 
     public SDKResultCallback(Promise promise) {
         this.resultPromise = promise;
@@ -25,10 +21,10 @@ class SDKResultCallback implements MailRuCallback {
         Log.i("ON_RESULT_RESULT", authResult.getCodeVerifier());
         Log.i("ON_RESULT_RESULT", authResult.getAuthCode());
 
+        WritableMap result = null;
         result.putString("codeVerifier", authResult.getCodeVerifier());
         result.putString("authCode", authResult.getAuthCode());
         this.resultPromise.resolve(result);
-        result = null;
     }
 
     @Override
