387184fbbedf545bd277f2d0383cd2ce02a138ce
Alexander Blokhin <sashablokhindev@gmail.com>
2021-03-23 17:11:33 +0300

    Added mail auth implementation

--- ios/MailOauth.h
+++ ios/MailOauth.h
@@ -1,5 +1,11 @@
 #import <React/RCTBridgeModule.h>
+#import <MRMailSDK/MRMailSDK.h>
 
-@interface MailOauth : NSObject <RCTBridgeModule>
+@interface MailOauth : NSObject <RCTBridgeModule, MRMailSDKDelegate>
+
+- (void)initialize;
+
+- (void)logIn:(RCTPromiseResolveBlock)resolve
+     rejecter:(RCTPromiseRejectBlock)reject;
 
 @end
--- ios/MailOauth.m
+++ ios/MailOauth.m
@@ -1,14 +1,40 @@
 #import "MailOauth.h"
 
-
 @implementation MailOauth
+
+    RCTPromiseResolveBlock mailResolveBlock;
+    RCTPromiseRejectBlock mailRejectBlock;
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+RCT_EXPORT_METHOD(initialize) {
+    NSDictionary *info = NSBundle.mainBundle.infoDictionary;
+    
+    MRMailSDK *mailSDK = [MRMailSDK sharedInstance];
+    [mailSDK initializeWithClientID:info[@"MailAppID"]
+                        redirectURI:info[@"MailRedirect"]];
+    mailSDK.returnScheme = info[@"MailReturnScheme"];
+    mailSDK.delegate = self;
+}
+
+
+RCT_EXPORT_METHOD(logIn:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    mailResolveBlock = resolve;
+    mailRejectBlock = reject;
+    
+    [MRMailSDK.sharedInstance authorize];
+}
+
+
+- (void)mrMailSDK:(MRMailSDK *)sdk authorizationDidFinishWithResult:(MRSDKAuthorizationResult *)result {
+    mailResolveBlock(@{
+        @"authCode": result.authorizationCode,
+    });
+}
+
+- (void)mrMailSDK:(MRMailSDK *)sdk authorizationDidFailWithError:(NSError *)error {
+    mailRejectBlock(@"AuthorizationDidFail", error.localizedDescription, error);
 }
 
 @end
